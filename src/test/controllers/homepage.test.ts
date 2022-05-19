17fb0016660da78d8a227e9c553d52f95c6c058c
Roger Urich <roger@itsovertime.com>
2022-05-19 04:29:06 -0700

    update tests

--- src/test/controllers/homepage.test.ts
+++ src/test/controllers/homepage.test.ts
@@ -1,15 +1,15 @@
 import request from 'supertest'
-import app from '../app'
-import { getRoute } from '../router'
+import app from '../../app'
+import { getRoute } from '../../router'
 
-test('Unauthorized page works', async () => {
+test('Unauthorized page throws 403', async () => {
   const url = getRoute('home page protected')
   const response = await request(app.callback()).get(url)
   expect(response.status).toBe(403)
   expect(response.text).toMatchSnapshot()
 })
 
-test('landing page un-protected works', async () => {
+test('Un-protected page returns a response', async () => {
   const url = getRoute('landing page un-protected')
   const response = await request(app.callback()).get(url)
   expect(response.status).toBe(200)
