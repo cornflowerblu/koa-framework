293c84b7de062a487c264993129da8b027c9b45b
Roger Urich <roger@itsovertime.com>
2022-05-18 19:03:38 -0700

    prettier

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,21 @@
 import invariant from 'tiny-invariant'
 
 type Routes = {
-    name: string
-    path: string
+  name: string
+  path: string
 }
 
 const routes: Array<Routes> = [
-    {
-        name: 'home',
-        path: '/',
-    },
+  {
+    name: 'home',
+    path: '/',
+  },
 ]
 
 const getRoute = (name: string): string => {
-    let routeName = routes.find((el) => el.name === name)
-    invariant(routeName)
-    return routeName?.path
+  let routeName = routes.find((el) => el.name === name)
+  invariant(routeName)
+  return routeName?.path
 }
 
 export default getRoute
