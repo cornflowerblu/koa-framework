382534a770f0fec63633d55b2a4f3bbfe0243265
Roger Urich <roger@itsovertime.com>
2022-05-18 17:40:42 -0700

    updated location of test directory

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import app from '../src/app'
+import app from '../app'
 
 test('Hello world works', async () => {
     const response = await request(app.callback()).get('/')
